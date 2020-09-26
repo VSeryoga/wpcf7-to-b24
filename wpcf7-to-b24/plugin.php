778ccbf1bef04666e865f02cc022a4c712673251
HOME <newtroick@mail.ru>
2020-09-26 19:38:44 +0500

    'One'

---
+++ wpcf7-to-b24/plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Передача данных в Битрикс24
+Description: Плагин предает данные из форм в Битрикс24
+Author: VSeryoga
+Author URI: https://vk.com/vseryoga/
+Version: 0.1.0.1
+*/
+
+require_once plugin_dir_path(__FILE__) . 'includes/wpcf7-to-b24_functions.php';
